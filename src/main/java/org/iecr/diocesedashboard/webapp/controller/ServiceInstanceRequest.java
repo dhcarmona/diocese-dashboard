2cd017ee18b55398606da5e8f329871478a6dcb8
Diego Carmona <dhcarmona@gmail.com>
2026-03-19 21:32:37 -0600

    (internal) Fix tests

--- src/main/java/org/iecr/diocesedashboard/webapp/controller/ServiceInstanceRequest.java
+++ src/main/java/org/iecr/diocesedashboard/webapp/controller/ServiceInstanceRequest.java
@@ -12,16 +12,16 @@ import java.util.List;
  * Request body for submitting a new Service Instance against a Service Template.
  */
 public record ServiceInstanceRequest(
-  @NotBlank String churchName,
-  List<Long> celebrantIds,
-  @NotNull @PastOrPresent LocalDate serviceDate,
-  @Valid List<ResponseEntry> responses) {
+@NotBlank String churchName,
+List<Long> celebrantIds,
+@NotNull @PastOrPresent LocalDate serviceDate,
+@Valid List<ResponseEntry> responses) {
 
   /**
    * A single answer to one Service Info Item question.
    */
   public record ResponseEntry(
-    @NotNull Long serviceInfoItemId,
-    @NotBlank String responseValue) {
+  @NotNull Long serviceInfoItemId,
+  @NotBlank String responseValue) {
   }
 }
