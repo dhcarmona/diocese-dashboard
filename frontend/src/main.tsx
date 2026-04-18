d8d73bd969592b94ba80e8b2540fb4567366ceac
Diego Carmona <dhcarmona@gmail.com>
2026-04-18 11:58:34 -0600

    (internal) Remove rubber banding on overscroll

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
+import GlobalStyles from '@mui/material/GlobalStyles'
 import { ThemeProvider } from '@mui/material/styles'
 import './i18n.ts'
 import App from './App.tsx'
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <GlobalStyles styles={{ 'html, body': { overscrollBehaviorY: 'none' } }} />
       <App />
     </ThemeProvider>
   </StrictMode>,
